7188b62da4ff03b8c43c9bd7de05d05d105ee887
shahid <baigs108@gmail.com>
2024-04-08 13:37:37 +0800

    ui done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WalletModal from "./Component/Modal/index";
+import Footer from "./Component/footer/Footer";
+import Footer1 from "./Component/footer1/App";
+import Header from "./Component/Header";
+import Home from "./pages/Home";
+import Artwork from "./pages/Artwork";
+import Browse from "./pages/Browse";
+import Shop from "./pages/Shop";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainn">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/connectwallet" element={<WalletModal />} />
+          <Route path="/artwork" element={<Artwork />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+        <Footer1/>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NftMarketplaceHome from "../../Component/marketPlaceHome/NftMarketplaceHome";
+import Artwork from "../../Component/ArtWorkSection/Artwork";
+import EconomySection from "../../Component/EconomySection";
+
+const Home = () => {
+  return (
+    <>
+      <NftMarketplaceHome />
+      <Artwork />
+      <EconomySection />
+     
+
+    </>
+  );
+};
+
+export default Home;
